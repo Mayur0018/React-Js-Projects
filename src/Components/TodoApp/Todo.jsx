48d4f343c51dcee8acdb8849dba2575d1118bad6
Mayur0018 <mayurnish18@gmail.com>
2025-07-14 22:05:26 +0530

    commit

--- src/Components/TodoApp/Todo.jsx
+++ src/Components/TodoApp/Todo.jsx
@@ -1,22 +1,53 @@
+import { useState } from "react";
 const Todolist = () => {
+  const [inputvalue, setInputvalues] = useState([]);
+  const [searchInput, setSearchinput] = useState("");
+  console.log(inputvalue);
+
+  const handleInputValue = (e) => {
+    setSearchinput(e.target.value);
+    console.log(searchInput);
+  };
+  const handleaddInputValue = (e) => {
+    e.preventDefault();
+    setInputvalues([...inputvalue, searchInput]);
+    setSearchinput("");
+  };
+  const handleDelete = (indexDelete) => {
+    setInputvalues(inputvalue.filter((_, elem) => elem !== indexDelete));
+  };
   return (
     <div>
-    <h1 className="bg-violet-300 text-white p-3 flex justify-center items-center w-screen h-[10vh] sm:h-[16vh] font-bold text-2xl sm:text-3xl md:text-4xl ">
-      Mayur To-Do List
-    </h1>
-
-    <form className='mt-10 flex flex-col md:flex-row md:justify-around'>
-      <input type="text" className='w-[60vw] md:w-[20vw] px-4 py-2 text-xl md:text-2xl border-2 m-2 mt-8 sm:m-5' placeholder='enter your task' 
-      
+      <h1 className="bg-violet-300 text-white flex justify-center items-center  h-[10vh] sm:h-[16vh] font-bold text-2xl sm:text-3xl md:text-4xl ">
+        Mayur To-Do List
+      </h1>
+      <input
+        type="text"
+        className="px-4 py-2 text-xl md:text-2xl border-2 m-2 mt-8 sm:m-5"
+        placeholder="enter your task"
+        value={searchInput}
+        onChange={handleInputValue}
       />
 
-       <input type="text" className='w-[60vw] md:w-[20vw] px-4 py-2 text-xl md:text-2xl border-2 m-2 mt-8 sm:m-5' placeholder='description of task' 
-      
-      />
-      <button className='bg-blue-700 text-xl md:text-2xl w-[35vw] md:w-[10vw] px-4 py-2 m-2 mt-8 sm:m-5 text-white'>Add Task</button>
-    </form>
+      <button
+        className="bg-blue-700 text-xl md:text-2xl w-[35vw] md:w-[10vw] px-4 py-2 m-2 mt-8 sm:m-5 text-white"
+        onClick={handleaddInputValue}
+      >
+        Add Task
+      </button>
+      {inputvalue.map((values, index) => (
+        <li key={index} className="flex  justify-around items-center">
+          <p className="mt-10 font-semibold text-2xl">{values}</p>
+          <button
+            className="bg-red-500 text-xl md:text-2xl w-[35vw] md:w-[10vw] px-4 py-2 m-2 mt-8 sm:m-5 text-white"
+            onClick={() => handleDelete(index)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Todolist;
